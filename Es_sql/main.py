db42f81f268ba7b8801397c47bdf39c5d251f299
annafirinu <anna.firinu2@outlook.it>
2025-11-26 18:07:37 +0100

    inserita nuova tabella
    inserita nuova tabella

--- Es_sql/main.py
+++ Es_sql/main.py
@@ -3,6 +3,61 @@ from analisi_posizionale import stampa_percentile
 from pulizia_dati import *
 import numpy as np
 import sqlite3
+import csv
+
+def leggi_colonne(file_path):
+    """
+    Legge il CSV e restituisce:
+    
+    header: lista dei nomi delle colonne
+    colonne: lista di liste, ogni lista contiene i valori numerici di una colonna"""
+    with open(file_path, 'r', newline='', encoding='utf-8') as f:
+       reader = csv.reader(f)
+       header = next(reader)  # salta intestazione
+       n_col = len(header)
+       colonne = [[] for _  in range(n_col)]  # crea una lista vuota per ogni colonna
+
+    for riga in reader:
+            for i in range(n_col):
+                val = riga[i].strip()  # rimuove spazi
+                try:
+                    val = float(val)     # converte in float
+                except:
+                    val = np.nan         # valori non numerici diventano NaN
+                colonne[i].append(val)
+
+    return header, colonne
+
+#------------------------------
+#Funzione: salva dataset e statistiche nel DB SQLite
+#------------------------------
+def salva_dataset_e_statistiche(header, colonne):
+    """
+    Crea due tabelle nel DB:
+    
+california_pulito -> dataset pulito
+california_statistiche -> min, max, media, mediana, deviazione standard per ogni colonna"""
+    conn = sqlite3.connect("housing.db")
+    cursor = conn.cursor()
+
+    # -------- Tabella dataset pulito --------
+    # Creiamo la definizione delle colonne
+    colonne_def = ", ".join([f"{col} REAL" for col in header])
+
+    # Rimuove la tabella precedente se esiste
+    cursor.execute("DROP TABLE IF EXISTS california_pulito")
+
+    # Crea tabella nuova
+    cursor.execute(f"CREATE TABLE california_pulito ({colonne_def})")
+
+    # Trasposta: da colonne -> righe
+    righe = list(zip(colonne))
+
+    # Inserisce tutte le righe nella tabella
+    cursor.executemany(
+        f"INSERT INTO california_pulito ({', '.join(header)}) VALUES ({', '.join(['?']*len(header))})",
+        righe
+    )
 
 #1. Caricare il CSV direttamente in NumPy
 data = np.genfromtxt("california_housing_data.csv", delimiter=",", skip_header=1)
